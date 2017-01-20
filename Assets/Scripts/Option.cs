dc1189f07ec69e714e32e6809918bbba04678c6b
Timothy <oddalicious@gmail.com>
2017-01-20 23:25:10 +1100

    Added GameManager.cs

---
+++ Assets/Scripts/Option.cs
@@ -0,0 +1,14 @@
+﻿public class Option {
+	public static int DEFAULT_INDEX = -99;
+	public string text;
+	public bool selected = false;
+	public int playerID = DEFAULT_INDEX;
+
+	public static Option GenerateEmptyOption() {
+		Option temp = new Option();
+		temp.text = "---";
+		temp.selected = false;
+		temp.playerID = DEFAULT_INDEX;
+		return temp;
+	}
+}
