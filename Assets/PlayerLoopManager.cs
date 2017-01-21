acd68865c524ab87d4c5f137aa67bc1c0c9b79f0
jhyslop01 <jhyslop01@gmail.com>
2017-01-21 11:46:26 +1100

    display some options for the first player in the player loop scene

--- Assets/PlayerLoopManager.cs
+++ Assets/PlayerLoopManager.cs
@@ -10,10 +10,17 @@ public class PlayerLoopManager : MonoBehaviour {
 	public Canvas passDeviceCanvas;
 	public Text playerActionText;
 
+	public Canvas optionsCanvas;
+	public Text optionText;
 
 	// Private variables
 
 	private int playersTurn;
+
+	private List<string> hexColours = new List<string> {"#f1c40f", "#2ecc71", "#3498db", "#9b59b6", "#e67e22", "#1abc9c", "#e74c3c"};
+
+	//TODO: delete this once we read from file..
+	private List<string> options = new List<string> {"Hug her!", "Do a cool dance", "Sleep", "Do some programming", "Watch TV"};
 
 
 	// Common methods
@@ -31,5 +38,23 @@ public class PlayerLoopManager : MonoBehaviour {
 	}
 
 
-	//
+	// Button actions methods
+
+	public void displayOptionsForUser() {
+		passDeviceCanvas.gameObject.SetActive(false);
+		optionsCanvas.gameObject.SetActive(true);
+
+		//TODO: display more or less options depending on the player difficulty
+		int maxOptions = options.Count;
+		for (int i = 0; i < maxOptions; i++) {
+			Vector2 position = new Vector2(0, 300 - (i * 100));
+
+			var option = Instantiate(optionText, position, Quaternion.identity);
+
+			//TODO: update the option text to be read from file...
+			option.text = string.Format("<color={0}>{1}</color>", hexColours[i % hexColours.Count], options[i]);
+
+			option.transform.SetParent(optionsCanvas.transform, false);
+		}
+	}
 }
